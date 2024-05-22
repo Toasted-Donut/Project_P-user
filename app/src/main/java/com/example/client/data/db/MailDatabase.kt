9dd63f29fa2240b6a4b554657a9e8ba3c4df1c9c
Toasted-Donut <Toasted-Donut@yandex.ru>
2024-05-22 13:29:22 +0300

    db class implemented

---
+++ app/src/main/java/com/example/client/data/db/MailDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.client.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.client.data.daos.CommonDao
+import com.example.client.data.models.Category
+import com.example.client.data.models.Check
+import com.example.client.data.models.CheckItem
+import com.example.client.data.models.Product
+
+
+@Database(entities = [Check::class, CheckItem::class, Product::class, Category::class], version = 1, exportSchema = false)
+abstract class MailDatabase : RoomDatabase() {
+
+    abstract fun getDao(): CommonDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MailDatabase? = null
+
+        private const val DB_NAME = "mail_database.db"
+
+        fun getDatabase(context: Context): MailDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MailDatabase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
